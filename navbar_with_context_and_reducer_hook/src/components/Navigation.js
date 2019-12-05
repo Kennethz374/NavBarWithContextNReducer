ec48781ef3fe06f4580084a3da2e786bbd0d851d
Kenneth <sally@sallyde-MacBook-Air.local>
2019-12-05 01:06:39 -0800

    Create a simple navigation

---
+++ navbar_with_context_and_reducer_hook/src/components/Navigation.js
@@ -0,0 +1,37 @@
+import React from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  InputBase,
+  SearchBase,
+  Switch,
+  withStyles
+} from '@material-ui/core'
+import {  SearchIcon } from '@material-ui/icons'
+import { mergeClasses } from "@material-ui/styles";
+
+export default function Navigation() {
+  return (
+    <div className = {classes.root}>
+      <AppBar position="static" color="primary">
+        <Toolbar>
+          <IconButton className={classes.menuButton} color="inherit">
+            <span>🇨🇳</span>
+          </IconButton>
+          <Typography variant="h6" className={classes.title} color="inherit">
+            Simple Navigation Bar
+          </Typography>
+          <Switch />
+          <div className={classes.grow} />
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
+            </div>
+          </div>
+        </Toolbar>
+      </AppBar>
+    </div>
+  )
+}
