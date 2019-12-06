518e333b1365ef0499b16bed7a350ef81b9ca4c9
Kenneth <sally@sallyde-MacBook-Air.local>
2019-12-06 13:22:03 -0800

    create a new component

--- navbar_with_context_and_reducer_hook/src/App.js
+++ navbar_with_context_and_reducer_hook/src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import './App.css';
 import Navigation from "./components/Navigation"
+import Form from "./components/Form"
 
 function App() {
   return (
     <>
-      <Navigation/>
+      {/* <Navigation/> */}
+      <Form />
+      
     </>
   );
 }
---
+++ navbar_with_context_and_reducer_hook/src/components/Form.js
@@ -0,0 +1,26 @@
+import React from "react"
+import {
+  Checkbox,
+  Avatar,
+  Button, 
+  CssBaseline, 
+  FormControl, 
+  FormControlLabel,
+  Input, 
+  InputLabel, 
+  Paper, 
+  Typography, 
+  MenuItem, 
+  Select
+} from "@material-ui/core"
+import LockOutlineIcon from "@material-ui/icons/LockOutlined"
+import useStyles from "../Styles/FormStyles"
+
+export default function Form() {
+  const classes = useStyles();
+  return (
+    <main className={classes.main}>
+      <h1>123242342342342</h1>
+    </main>
+  )
+}
