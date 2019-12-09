a8bbb7977edcb09e24e5b0464d75360534a41e8f
Kenneth <sally@sallyde-MacBook-Air.local>
2019-12-08 21:12:39 -0800

    completed the simple nav

--- navbar_with_context_and_reducer_hook/src/components/Form.js
+++ navbar_with_context_and_reducer_hook/src/components/Form.js
@@ -3,7 +3,6 @@ import {
   Checkbox,
   Avatar,
   Button, 
-  CssBaseline, 
   FormControl, 
   FormControlLabel,
   Input, 
--- navbar_with_context_and_reducer_hook/src/components/PageContent.js
+++ navbar_with_context_and_reducer_hook/src/components/PageContent.js
@@ -2,7 +2,7 @@ import React, {useContext} from 'react';
 import {ThemeContext} from "../contexts/ThemeContext"
 
 export default function PageContent(props) {
-  const {isDarkMode, toggleTheme} = useContext(ThemeContext)
+  const {isDarkMode} = useContext(ThemeContext)
   const styles = {
     backgroundColor: isDarkMode? "black":"white",
     height: '100vh',
